dd73227561f79454919d6c09aa9c4fb4be86421a
bsingh0517 <85919103+bsingh0517@users.noreply.github.com>
2025-02-01 22:47:07 -0600

    Add project files.

---
+++ LoginForm/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace LoginForm
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnClose = new Button();
+            btnValidate = new Button();
+            lblUsername = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnClose
+            // 
+            btnClose.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClose.Location = new Point(464, 312);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(219, 69);
+            btnClose.TabIndex = 0;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnValidate
+            // 
+            btnValidate.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnValidate.Location = new Point(36, 312);
+            btnValidate.Name = "btnValidate";
+            btnValidate.Size = new Size(219, 69);
+            btnValidate.TabIndex = 1;
+            btnValidate.Text = "Validate";
+            btnValidate.UseVisualStyleBackColor = true;
+            btnValidate.Click += btnValidate_Click;
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblUsername.Location = new Point(29, 112);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(209, 48);
+            lblUsername.TabIndex = 2;
+            lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPassword.Location = new Point(36, 204);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(202, 48);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtUsername.Location = new Point(254, 112);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(429, 56);
+            txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtPassword.Location = new Point(254, 196);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(429, 56);
+            txtPassword.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DeepSkyBlue;
+            label1.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Indigo;
+            label1.Location = new Point(94, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(515, 48);
+            label1.TabIndex = 6;
+            label1.Text = "Welcome to my Login page!";
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.IndianRed;
+            ClientSize = new Size(712, 465);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(btnValidate);
+            Controls.Add(btnClose);
+            ForeColor = Color.Black;
+            Name = "frmLogin";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnClose;
+        private Button btnValidate;
+        private Label lblUsername;
+        private Label lblPassword;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Label label1;
+    }
+}
---
+++ LoginForm/Form1.cs
@@ -0,0 +1,41 @@
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace LoginForm
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            string @username = "bibek.singh";
+            string @password = "217CC9268199117MaY!";
+            SqlConnection conn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True;TrustServerCertificate=True");
+            conn.Open();
+            String query = "SELECT COUNT(*) FROM Login_all WHERE username  = @username AND password = @password";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+            int count = (int) cmd.ExecuteScalar();
+            conn.Close();
+            if (count > 0)
+            {
+                MessageBox.Show("Login Success!");
+            }
+            else
+            {
+                MessageBox.Show("Login Error!");
+            }
+        }
+    }
+}
